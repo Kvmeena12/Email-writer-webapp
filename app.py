2a021ddaafd4c3b6598e72089d8b9a9c8952097e
Kvmeena12 <140626771+Kvmeena12@users.noreply.github.com>
2025-07-24 10:19:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.markdown(
 )
 
     # UI columns for the controls
-    col1, col2, col3, col4 = st.columns([2, 2, 2, 3])
+col1, col2, col3, col4 = st.columns([2, 2, 2, 3])
 
     with col1:
         selected_length = st.selectbox("Length", options=length_options)
